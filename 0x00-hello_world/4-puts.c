ecfe25ce7900bb79d26ffc156486d581df7926bf
osamanazar <osamanazar47@gmail.com>
2023-09-07 19:03:24 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-nclude <stdio.h>
+include <stdio.h>
 int main ()
 {
-	puts(""Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 			return 0;
 			}
