113cbcc0ffe0b2c4cc95c85f0476b8b5bef897c5
osamanazar <osamanazar47@gmail.com>
2023-09-07 19:32:16 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-include<stdio.h>
-int main ()
+#include <stdio.h>
+
+/**
+ * main - this is  main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-			return 0;
-			}
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}				
