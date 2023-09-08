80dd5d5629d01316fc02f0f33e176c05d3529124
osamanazar <osamanazar47@gmail.com>
2023-09-07 20:54:45 -0700

    understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the main library
+ *
+ * Return: 1 to indecate there is an errorr
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
