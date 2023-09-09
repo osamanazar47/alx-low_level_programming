25a36b07d3e8a13b28eb2e11d4b81017984feb0a
osamanazar <osamanazar47@gmail.com>
2023-09-09 10:31:40 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+	char alp[24] = "abcdfghijklmnoqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n')
+		return (0);
+}
