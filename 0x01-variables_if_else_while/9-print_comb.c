d88f1745bff8aa4e60fe5fba1dda716d48058f4d
osamanazar <osamanazar47@gmail.com>
2023-09-09 12:37:08 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- the main function for printing the combinations
+ *
+ * Return: 0 as before
+ */
+int main(void)
+{
+	int i;
+
+	for (i =48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
