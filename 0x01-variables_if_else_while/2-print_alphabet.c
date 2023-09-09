06e32724a441669248dee0cdf2b158600dbd37d6
osamanazar <osamanazar47@gmail.com>
2023-09-09 09:18:10 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: to 0
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz"
+		int i;
+	for (i = 0, i < 26, i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
