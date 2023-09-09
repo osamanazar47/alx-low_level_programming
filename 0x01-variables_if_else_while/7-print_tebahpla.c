6a44f65860b5d1d479af67575e4861e14a3199d7
osamanazar <osamanazar47@gmail.com>
2023-09-09 11:19:18 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function for printing reverse alphabets
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= a; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
