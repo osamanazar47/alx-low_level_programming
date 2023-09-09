da77ddd0dc44a70d23fe136c9eef0e19d2b07bb4
osamanazar <osamanazar47@gmail.com>
2023-09-09 10:39:02 -0700

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-	char alp[24] = "abcdfghijklmnoqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 24; i++)
+	for (i = 97; i < 123; i++)
 	{
-		putchar(alp[i]);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
-	putchar('\n')
-		return (0);
+	putchar('\n');
+	return (0);
 }
