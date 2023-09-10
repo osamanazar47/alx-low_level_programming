d96ce030a67631f13ae1bc20b18ad8bb57e39db2
osamanazar <osamanazar47@gmail.com>
2023-09-10 01:29:07 -0700

    more brains

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - the function that prints all combinations with specific rules
+ *
+ * Return: 0 is for success
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = i + 1; j <= 9; j++)
+		{
+			if (i < j)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar(',');
+				putchar(' ');
+				putchar('\n');
+			}
+		}
+	}
+	return (0);
+}
