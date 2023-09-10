1618a777aa9ef80fd4b68409a94be7bbce2cddb1
osamanazar <osamanazar47@gmail.com>
2023-09-10 02:34:35 -0700

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-	int first_combination = 1;
+	int i, j, k, first_combination = 1;
 
-	for (int i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (int j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			for (int k = j + 1; k <= 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				if (i < j && j < k)
 				{
