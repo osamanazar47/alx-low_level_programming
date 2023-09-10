13dd370907dd01c2297212efd71a7d8b74c8e56b
osamanazar <osamanazar47@gmail.com>
2023-09-10 01:41:01 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,9 +6,11 @@
  */
 int main(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (int j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			if (i < j)
 			{
