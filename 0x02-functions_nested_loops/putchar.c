d9ab1558bc83c9b1bc52554230db1a2e6bc9b12c
osamanazar <osamanazar47@gmail.com>
2023-09-12 12:59:20 -0700

    fixing implementation

--- 0x02-functions_nested_loops/putchar.c
+++ 0x02-functions_nested_loops/putchar.c
@@ -49,6 +49,18 @@ int _abs(int)
 {
 }
 
+#include "main.h"
+
 int print_last_digit(int)
 {
+	int last_digit;
+
+	if (i < 0)
+	{
+		last_digit = -n % 10;
+	}
+	else
+	{
+		last_digit = n % 10;
+	}
 }
