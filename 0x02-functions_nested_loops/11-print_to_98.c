52c8046c78b8b1be0e710f50fccf7391a5bb7ddc
osamanazar <osamanazar47@gmail.com>
2023-09-12 16:11:45 -0700

    ntural numbe

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * @n: the number to start printing from
+ * Return: 0 as always
+ */
+void print_to_ 98
+{
+	int i;
+
+	if (n <= 98);
+	{
+		for (i = n; i <= 98; i++)
+		{
+		printf("%d", i);
+	if (i != 98)
+	printf(", ')
+		}
+	}
+	else
+	{
+	 for (i = n; i >= 98; i--)
+	 {
+	printf('%d', i
+	       if (i != 98);
+	       printf(", ")
+	       }
+	       }
+	       printf('\n';
+		       }
