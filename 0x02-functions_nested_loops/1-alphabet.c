ac4f7682eeda0ce4b8097f979e2d58d2849431c6
osamanazar <osamanazar47@gmail.com>
2023-09-12 02:31:47 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - a function for printing the alphabets
+ *
+ * Return: 0 as always
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/putchar.c
+++ 0x02-functions_nested_loops/putchar.c
@@ -4,3 +4,14 @@ int _putchar(char c)
 {
 	return write(1, &c, 1);
 }
+
+void print_alphabet(void)
+{
+	char letter = 'a'
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	_putchar('\n');
+}
