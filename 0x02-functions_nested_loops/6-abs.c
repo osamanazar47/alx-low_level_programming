635e29baeaaa88d3f33ae898c1eba0e72e9a9791
osamanazar <osamanazar47@gmail.com>
2023-09-12 12:08:50 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * @n:the function that we will compute
+ * _abs - a function that computes the absolute value of an integer
+ * Return: o for success
+ */
+int _abs(int n)
+{
+	if (n >= o)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
--- 0x02-functions_nested_loops/putchar.c
+++ 0x02-functions_nested_loops/putchar.c
@@ -44,3 +44,7 @@ int _isalpha(int c)
 int print_sign(int n)
 {
 }
+
+int _abs(int)
+{
+}
