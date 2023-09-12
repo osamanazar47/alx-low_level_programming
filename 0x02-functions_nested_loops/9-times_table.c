239903c8b66a0bb32fd36e289df917746b0dd9af
osamanazar <osamanazar47@gmail.com>
2023-09-12 15:31:53 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - a function that prints the 9 times table
+ *
+ * Return: 0 as always
+ */
+void times_table(void)
+{
+	int i, j = 9, result;
+
+	for (i = 0; i <= 10; i++)
+	{
+		result = i * j;
+
+		putchar(i + '0');
+		putchar(' ');
+		putchar('*');
+		putchar(' ');
+		putchar(j + '0');
+		putchar(' ');
+		putchar('=');
+		putchar(' ');
+		putchar(result + '0');
+		putchar('\n');
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/putchar.c
+++ 0x02-functions_nested_loops/putchar.c
@@ -66,5 +66,9 @@ int print_last_digit(int)
 }
 
 void jack_bauer(void)
+{
+}
 
+void times_table(void)
+{
 }
