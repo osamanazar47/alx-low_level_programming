708c519810604bc3ba1127048fd6457dc21bf52f
osamanazar <osamanazar47@gmail.com>
2023-09-12 15:43:45 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,33 @@
  */
 void times_table(void)
 {
-	int i, j = 9, result;
+	int x, y, z, u, d;
 
-	for (i = 0; i <= 10; i++)
+	for (x = 0; x <= 9; x++)
 	{
-		result = i * j;
-
-		putchar(i + '0');
-		putchar(' ');
-		putchar('*');
-		putchar(' ');
-		putchar(j + '0');
-		putchar(' ');
-		putchar('=');
-		putchar(' ');
-		putchar(result + '0');
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
+		}
 		putchar('\n');
 	}
-	return (result);
 }
