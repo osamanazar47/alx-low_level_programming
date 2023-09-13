cbcdaf024478c14c83712e41e2f456c83f29e0f2
osamanazar <osamanazar47@gmail.com>
2023-09-13 04:35:03 -0700

    m

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the main library
+ *
+ * Return: o as always
+ */
+void positive_or_negative(int n)
+{
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+	       	printf("%d is zero\n", n);
+	}
+}
