afe9d07eaba5b02e43c4d52fbe44316c0bd08604
osamanazar <osamanazar47@gmail.com>
2023-09-14 11:38:00 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - printing fizzbuzz
+ * Return: o for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("fizzbuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
