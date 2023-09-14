6abfaecbbf4644aa167e0050b66269bfd239fd24
osamanazar <osamanazar47@gmail.com>
2023-09-14 04:02:35 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,10 @@ void print_numbers(void)
 {
 	int num = 0;
 
-	do{
-		_putchar(num + 48);
+	do {
+		_putchar(num + '0');
 		num++;
-	}while (num >= 0 && num <= 9);
+	} while (num <= 9);
+
 	_putchar('\n');
 }
