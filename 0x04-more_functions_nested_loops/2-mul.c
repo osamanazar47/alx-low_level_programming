0b7907711fb4f228c05570c0eb0f1f6280351082
osamanazar <osamanazar47@gmail.com>
2023-09-14 03:16:16 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,13 +3,9 @@
  * mul - multiple two integers
  * @a: first integer
  * @b: second integer
- * Return: 0 for success
+ * Return: result of the multiplicattion
  */
 int mul(int a, int b)
 {
-	int mult;
-
-	mult = a * b;
-	_putchar(mult + '0');
-	return (mult);
+	return (a * b);
 }
