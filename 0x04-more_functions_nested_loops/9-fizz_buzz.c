280a2f9e2309c70a698d40e89953575f4e496bd8
osamanazar <osamanazar47@gmail.com>
2023-09-14 11:59:04 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,29 +10,18 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && !(i % 5 == 0))
-		{
 			printf("fizz");
-		}
 		else if (i % 5 == 0 && !(i % 3 == 0))
-		{
 			printf("buzz");
-		}
 		else if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("fizzbuzz");
-		}
 		else
-		{
 			printf("%d", i);
-		}
+		
 		if (i != 100)
-		{
 			printf(" ");
-		}
 		else
-		{
 			printf("\n");
-		}
 	}
 
 	return (0);
