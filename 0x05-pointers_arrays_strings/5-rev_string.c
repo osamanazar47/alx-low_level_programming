b48b9f3f05c876cffbd8efb7bdf0d3934a9df05c
osamanazar <osamanazar47@gmail.com>
2023-09-18 08:56:24 -0700

    string rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - reverse the string
+ * @s: the string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	strrev(s);
+}
