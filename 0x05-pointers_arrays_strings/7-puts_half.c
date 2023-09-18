ab347b55287b50797e5d2ddb2e0bc5bd9399e293
osamanazar <osamanazar47@gmail.com>
2023-09-18 11:35:31 -0700

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,11 @@ void puts_half(char *str)
 
 	if (length % 2 == 0)
 	{
-		start_index = length / 2;
+		start_index = (length - 1) / 2;
 	}
 	else
 	{
-		start_index = (length - 1) / 2;
+		start_index = ((length - 1) - 1) / 2;
 	}
 	for (i = start_index; i < length; i++)
 	{
