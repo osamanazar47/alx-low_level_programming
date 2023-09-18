530f9565c4ada5524d04a47b9570055baf9ae9b8
osamanazar <osamanazar47@gmail.com>
2023-09-18 06:50:57 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - return the length of a string
+ * @s: the string
+ * Return: the length of the of the string
+ */
+int _strlen(char *s)
+{
+	size_t length = strlen(s);
+}
