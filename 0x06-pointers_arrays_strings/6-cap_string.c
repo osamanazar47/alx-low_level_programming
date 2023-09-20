768cb5fc8bc19b94fb4df1b0ca16ff71643161ed
osamanazar <osamanazar47@gmail.com>
2023-09-20 13:25:39 -0700

    caps

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * cap_string - capitalizes all the words of a string
- * @str: the string which wiil be capitalized
- * Return: str
+ * *cap_string - Capitalizes all words of a string
+ * @str: the string to be capitalized
+ *
+ * Return: the modified string
  */
 char *cap_string(char *str)
 {
-	int i;
+	int num = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[num])
 	{
-		if (i == 0)
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32;
-		}
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-		 str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-		 str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
-		 str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
-		 str[i - 1] == '}')
-		{
-			str[i] = str[i] - 32;
-		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
-		}
+		while (!(str[num] >= 'a' && str[num] <= 'z'))
+			num++;
+
+		if (str[num - 1] == ' ' || str[num - 1] == '\t' ||
+		str[num - 1] == '\n' || str[num - 1] == ',' || str[num - 1] == '.' ||
+		str[num - 1] == ';' || str[num - 1] == '!' || str[num - 1] == '?' ||
+		str[num - 1] == '"' || str[num - 1] == '(' || str[num - 1] == ')' ||
+		str[num - 1] == '{' || str[num - 1] == '}' || num == 0)
+			str[num] -= 32;
+		num++;
 	}
 	return (str);
 }
