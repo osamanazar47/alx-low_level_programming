0380f6b5005bb08f6235e33dedee118ddebee21a
osamanazar <osamanazar47@gmail.com>
2023-09-20 16:02:50 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,6 +8,7 @@
 void print_number(int n)
 {
 	unsigned int num;
+
 	num = n;
 
 	if (n < 0)
@@ -17,7 +18,7 @@ void print_number(int n)
 	}
 	if (num / 10 != 0)
 	{
-		print_nummber(num / 10);
+		print_number(num / 10);
 	}
 	_putchar('0' + (num % 10));
 }
