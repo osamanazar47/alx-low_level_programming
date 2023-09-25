a80f1268e24b6205e55acb2d244dffbe27cb2691
osamanazar <osamanazar47@gmail.com>
2023-09-25 06:49:27 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
-		i++;
 	}
-	return (NULL);
+	return (0);
 }
