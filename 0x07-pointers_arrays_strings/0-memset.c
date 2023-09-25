31cc435a8754efd0806b155c42046889b6c7aafd
osamanazar <osamanazar47@gmail.com>
2023-09-25 05:42:15 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - fills specified memory location with a constant byte
+ * @s: the pointer to the memory
+ * @b: the constant byte
+ * @n: the number of bytes filled with the constant
+ * Return: pointer to the memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = 'b';
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+project in pointers,arrays and strings
+this project is focusd on the pointer to a pointer and two dimentional concepts
