a011f953906adafe4137e2db1e1b44bbf0c9806a
osamanazar <osamanazar47@gmail.com>
2023-09-25 12:36:33 -0700

    diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,7 +15,10 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i * size + i];
-		sum2 += a[i * size + (size - 1 - i)];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		sum2 += a[i * size + (size - i - 1)];
 	}
 	printf("%d, %d", sum1, sum2);
 }
