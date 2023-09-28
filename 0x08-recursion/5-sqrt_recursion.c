0e4aeca6af410f85429ab948c2ef2f97e27851b2
osamanazar <osamanazar47@gmail.com>
2023-09-28 06:14:37 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,18 +3,25 @@
 /**
  * _sqrt_recursion - function that returns the natural square rootof a number
  * @n: the number
- * @i: the guess
  * Return: the natural square root of n
  */
-int _sqrt_recursion(int n, int i)
+int _sqrt_recursion(int n)
 {
-	if (i < 0)
+	if (n < 0)
 		return (-1);
-	if (i == n)
-		return (-1);
-	i = n / 10;
-
+	return (_sqrt_recursive(n, n / 10));
+}
+/**
+ * _sqrt_recursive - a recursive function that finds the square root
+ * @n: the number
+ * @i: the guess
+ * Return: the natural square root
+ */
+int _sqrt_recursive(int n, int i)
+{
 	if (i * i == n)
 		return (i);
-	return (_sqrt_recursion(n, i + 1));
+	if (i * i > n)
+		return (-1);
+	return (_sqrt_recursive(n, i + 1));
 }
