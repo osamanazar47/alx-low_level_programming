c6d5e4e904a5316694a30ce9cf580ad1a5d19865
osamanazar <osamanazar47@gmail.com>
2023-10-02 07:16:55 -0700

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
  * main - function for printing the name of the program
  * @argc: for number of arguments
  * @argv: an array of strings of the arguments
+ * Return: o for success
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	
 	puts(argv[0]);
 	return (0);
 }
