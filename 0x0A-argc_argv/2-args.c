74ff2d65cd2660c10768ff94ba4223e500aa1a2d
osamanazar <osamanazar47@gmail.com>
2023-10-02 07:38:52 -0700

    argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prits all arguments it receives
+ * @argc: the number of arguments
+ * argv: an array of arguments as strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		puts(argv[i]);
+	}
+	return (0);
+}
