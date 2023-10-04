ccc31850decc1d2e3421e389c3fc0b0efef6b344
osamanazar <osamanazar47@gmail.com>
2023-10-04 03:22:43 -0700

    create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,14 +9,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
 	char *ptr;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	ptr = (char*)malloc(size * sizeof(char));
+	ptr = (char *)malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
