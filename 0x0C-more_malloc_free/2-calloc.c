51d35fcae2ad15776e452f77fd239d33efa7552e
osamanazar <osamanazar47@gmail.com>
2023-10-06 14:50:48 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,10 +9,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
+	memset(arr, 0, nmemb * size);
 	return (arr);
 }
