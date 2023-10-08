36bad3321038a6837bb4a5fa6266ca02e3af6b16
osamanazar <osamanazar47@gmail.com>
2023-10-08 00:44:06 -0700

    mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - multiplies two positive numbers
+ * @argc: the number of arguments
+ * @argv:: an array of strings of arguments
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int num1, num2, mul;
+	int i, j;
+	char *dig;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		dig = argv[i];
+		j = 0;
+		while (dig[j] != '\0')
+		{
+			if (!isdigit(dig[j]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+			j++;
+		}
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	if (num1 <= 0 || num2 <= 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
+}
