173979c1667221b3d9978af7e925bc55040f7c28
osamanazar <osamanazar47@gmail.com>
2023-10-10 12:13:17 -0700

    print info

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,14 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
 	if (d->age == 0)
 	{
 		printf("(nil)\n");
