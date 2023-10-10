9283944b36c54167127bd98ac0fbe1c0d6b33f30
osamanazar <osamanazar47@gmail.com>
2023-10-10 14:52:39 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,6 +23,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog->name);
 		free(new_dog->owner);
 		free(new_dog);
+		return (NULL);
 	}
 	new_dog->age = age;
 	return (new_dog);
