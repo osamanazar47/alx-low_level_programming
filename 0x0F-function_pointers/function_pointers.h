85862e98b2a6cba1ba94870cc3de56cb3aa8b2c8
osamanazar <osamanazar47@gmail.com>
2023-10-11 04:58:36 -0700

    print

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+void print_name(char *name, void (*f)(char *));
+#include <unistd.h>
+void _putchar(char c)
+{
+	write(1, &c, 1);
+}
+#endif
