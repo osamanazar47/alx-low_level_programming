03cfb560648435d05fa53d2b6e05aff874776ebf
osamanazar <osamanazar47@gmail.com>
2023-10-12 06:12:42 -0700

    strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		if (i < n - 1)
 		{
-			if (separator != NULL)
+			if (separator != NULL && separator[0])
 				printf("%s", separator);
 		}
 	}
