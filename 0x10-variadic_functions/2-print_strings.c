2c698e8394257a86e065ba3c58b9cdce8c3a7a68
osamanazar <osamanazar47@gmail.com>
2023-10-12 06:44:02 -0700

    strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,7 +11,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
@@ -21,11 +24,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		if (i < n - 1)
-		{
-			if (separator != NULL && separator[i])
-				printf("%s", separator);
-		}
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(args);
