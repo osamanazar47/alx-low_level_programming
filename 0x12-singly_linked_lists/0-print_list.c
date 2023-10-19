2ba8315c34063cd2cce9504460edecaca57b2e93
osamanazar47 <osamanazar47@gmail.com>
2023-10-19 10:21:16 -0700

    list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,10 +9,12 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 	const list_t *current = h;
 
+	if (h == NULL)
+		return (0);
 	while (current != NULL)
 	{
 		if (current->str)
-			printf("[%lu] %s\n", current->len, current->str);
+			printf("[%d] %s\n", current->len, current->str);
 		else
 			printf("[0] (nil)\n");
 		count++;
