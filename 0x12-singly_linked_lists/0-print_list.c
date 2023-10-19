62c6483b684582dcfd2430380e8f87ed6df979b8
osamanazar47 <osamanazar47@gmail.com>
2023-10-19 10:00:32 -0700

    lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - prints the elements of the list
+ * @h: the pointer to the first node
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str)
+			printf("[%d] %s\n", current->len, current->str);
+		else
+			printf("[0] (nil)\n");
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this is a project for the linked list concept
