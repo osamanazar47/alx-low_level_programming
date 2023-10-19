dba3aa55b391fe69d4afd67e8c4db1b0291e0f4f
osamanazar47 <osamanazar47@gmail.com>
2023-10-19 11:47:05 -0700

    new

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,13 +8,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+	unsigned int i, count = 0;
 
 	if (new_node == NULL)
 		return (NULL);
 	if (str == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	new_node->len = strlen(new_node->str);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	new_node->len = count;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
