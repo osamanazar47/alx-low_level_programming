a01c1ab4014b743397c1c4fe5afe9587740dbbe9
osamanazar47 <osamanazar47@gmail.com>
2023-10-19 15:09:36 -0700

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * before_main - a function to executed before the main function is called
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
