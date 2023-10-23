d268b4da3fe31b53928c8315cefaedcdfc1705cf
osamanazar47 <osamanazar47@gmail.com>
2023-10-23 11:08:42 -0700

    free node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
+	if (!head)
+		return;
 	while (*head != NULL)
 	{
 		current = *head;
 		*head = (*head)->next;
 		free(current);
 	}
+	*head = NULL;
 }
