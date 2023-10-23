94a0bb76430ea3f71e4c3846a54be72a7ff60eb0
osamanazar47 <osamanazar47@gmail.com>
2023-10-23 11:14:55 -0700

    del head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,10 +9,11 @@ int pop_listint(listint_t **head)
 	listint_t *current;
 	int data;
 
-	if (*head == NULL)
+	if (!*head || !head)
 		return (0);
 	current = *head;
 	*head = (*head)->next;
 	data = current->n;
+	free(current);
 	return (data);
 }
