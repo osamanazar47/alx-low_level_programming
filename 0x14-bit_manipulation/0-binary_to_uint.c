64e5464283841953c85829f72a933f4d9fdc4165
osamanazar47 <osamanazar47@gmail.com>
2023-10-26 02:56:27 -0700

    bin

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@ unsigned int binary_to_unit(const char *b)
 	int i = 0, j = 0;
 	unsigned int sum = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 	while (b[i] != '\0')
 	{
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <limits.h>
 unsigned int binary_to_uint(const char *b);
 #endif
