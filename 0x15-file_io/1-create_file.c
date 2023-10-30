38556ed178288694787cfc3814f8587a2087ed99
osamanazar47 <osamanazar47@gmail.com>
2023-10-30 11:20:21 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 	text = write(file, text_content, strlen(text_content));
-	if (text == strlen(text_content))
+	if (strlen(text_content) == 0 || text == strlen(text_content))
 	{
 		close(file);
 		return (1);
