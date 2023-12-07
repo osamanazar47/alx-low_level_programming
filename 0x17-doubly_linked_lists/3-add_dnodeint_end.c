fe0dc6cb7ee990ea264bae754ea90d81fe734e18
osama <osamanazar47@gmail.com>
2023-12-07 15:36:46 -0800

    add node end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,7 +10,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *temp, *new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL || head == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
