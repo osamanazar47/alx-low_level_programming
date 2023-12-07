dab329d44a6edeed5ade56ec701e140048c6d351
osama <osamanazar47@gmail.com>
2023-12-07 15:01:20 -0800

    dlistint

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * dlistint_len - a function that returns the number of elements
+ * @h: a pointer to the first node
+ * Return: the number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		elements++;
+	}
+	return (elements)
