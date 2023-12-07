df35476e197cccd0ffc75f3c6721262938bddf37
osama <osamanazar47@gmail.com>
2023-12-07 14:56:57 -0800

    dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * print_dlistint - print the elements of the linked list
+ * @h: a pointer to the first node
+ * Return: the number of elements
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t element = 0;
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -19,3 +20,4 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
+#endif
