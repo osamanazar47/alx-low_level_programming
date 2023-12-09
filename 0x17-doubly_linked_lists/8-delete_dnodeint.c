ceae471129e214b37cb409324fb65fad8fa15c54
osama <osamanazar47@gmail.com>
2023-12-09 09:42:10 -0800

    del node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,9 +19,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (current->next != NULL)
 			current->next->prev = NULL;
 		free(current);
-		return (-1);
+		return (1);
 	}
-	while (current != NULL && idx != NULL)
+	while (current != NULL && idx != index)
 	{
 		idx++;
 		current = current->next;
