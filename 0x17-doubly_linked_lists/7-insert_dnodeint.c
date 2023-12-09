bd1c18660b56dfb9b9293c478ae2957cf4f36375
osama <osamanazar47@gmail.com>
2023-12-09 04:29:43 -0800

    insert at idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,24 +8,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode, *current = **h;
+	dlistint_t *newnode, *current;
 	unsigned int index = 0;
 
-	if (current == NULL)
+	if (h == NULL)
 		return (NULL);
+	current = *h;
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
+	if (idx == 0)
+	{
+		newnode->next = current;
+		newnode->prev = NULL;
+		if (current != NULL)
+			current->prev = newnode;
+		*h = newnode;
+		return (newnode);
+	}
 	while (current != NULL && index != idx)
 	{
 		index++;
 		current = current->next;
 	}
 	if (current == NULL)
+	{
+		free(newnode);
 		return (NULL);
+	}
 	newnode->next = current;
 	newnode->prev = current->prev;
 	current->prev = newnode;
+	if (newnode-> prev != NULL)
+		newnode->prev->next = newnode;
 	return (newnode);
 }
