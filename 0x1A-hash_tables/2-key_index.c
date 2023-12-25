bd4d08fd589028158eeefd72313240f4a91f13c8
osamanazar <osamanazar47@gmail.com>
2023-12-24 22:53:01 -0800

    index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives you the index of a key
+ * @key: the key you want the index of
+ * @size: the size of the array
+ * Return: the index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+	return (index);
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -6,3 +6,5 @@ this project is about hash tables in c language, how to create and use them(inse
 0-hash_table_create.c: a function that creates a hash table.
 
 1-djb2.c: a hash function implementing the djb2 algorithm.
+
+2-key_index.c - a function that gives you the index of a key.
