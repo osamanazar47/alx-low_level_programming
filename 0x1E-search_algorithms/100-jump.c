270f38cbf60d6d421627306ffa7e14c2fe79f483
osamanazar47 <osamanazar47@gmail.com>
2024-04-14 11:07:27 +0200

    jump_search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,42 +1,35 @@
 #include "search_algos.h"
 /**
- * jump_search - A function that searches for a value in a sorted array of
- * integers using jump search algorithm
- * @array: a pointer to the first elemet of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- * Return: the firstindex where value is located or -1 on failure
+* jump_search - A function that searches for a value in a sorted array of
+* integers using jump search algorithm
+* @array: a pointer to the first elemet of the array to search in
+* @size: the number of elements in array
+* @value: the value to search for
+* Return: the firstindex where value is located or -1 on failure
 */
 int jump_search(int *array, size_t size, int value)
 {
-	int f = 0, j = sqrt(size);
-	int l = j;
+	int step = sqrt(size);
+	int prev = 0, i;
 
 	if (array == NULL)
-	{
 		return (-1);
-	}
-	printf("Value checked array[%d] = [%d]\n", f, array[f]);
-	while (array[l] < value && l < (int)size)
+
+	while (array[prev] < value && prev < (int)size)
 	{
-		f += j;
-		l += j;
-		printf("Value checked array[%d] = [%d]\n", f, array[f]);
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		prev += step;
 	}
-	printf("Value found between indexes [%d] and [%d]\n", f, l);
-	if (array[f] < value && array[l] >= value && array[f] < (int)size)
+
+	printf("Value found between indexes [%d] and [%d]\n", prev - step, prev);
+
+	for (i = prev - step; i <= prev && i < (int)size; i++)
+
 	{
-		while (array[f] < value)
-		{
-			printf("Value checked array[%d] = [%d]\n", f, array[f]);
-			f += 1;
-		}
-		printf("Value checked array[%d] = [%d]\n", f, array[f]);
-		return (f);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
-	else
-	{
-		printf("Value checked array[%d] = [%d]\n", f, array[f]);
-		return (-1);
-	}
+
+	return (-1);
 }
